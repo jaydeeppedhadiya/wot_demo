cd014ddb88a34e9c911f5e2a1fb8311bfbaf3cb9
jaydeeppedhadiya89050 <jaydeeppedhadiya89050@gmail.com>
2020-09-15 15:28:43 +0530

    demo

---
+++ index.js
@@ -0,0 +1,42 @@
+const electron = require('electron')
+
+const { app, BrowserWindow, autoUpdater } = require('electron')
+const server = 'https://jaydeepwot@bitbucket.org/jaydeepwot/wot_electron.git'
+const url = `${server}/update/${process.platform}/${app.getVersion()}`
+console.log(url)
+autoUpdater.setFeedURL({ url })
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+setInterval(() => {
+    autoUpdater.checkForUpdates()
+  }, 10000)
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    }
+  
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall()
+    })
+  })
+  autoUpdater.on('error', message => {
+    console.error('There was a problem updating the application')
+    console.error(message)
+  })
